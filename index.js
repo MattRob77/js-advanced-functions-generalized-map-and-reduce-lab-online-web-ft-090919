8deeb7c0203f5f5cb7f3c6fe48d11b09773067ec
Matthew Robinson <matt_robinson1@yahoo.com>
2020-01-06 18:57:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Add your functions here
-function map (arr, func ) { 
-  
-} 
+function map (arr, func) {
+    let newArr = []
+    arr.forEach((item) => {
+        newArr.push(func(item))
+    })
+    return newArr
+}
