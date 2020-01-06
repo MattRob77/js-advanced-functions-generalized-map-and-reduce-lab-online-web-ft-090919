a5a6b28763097fa6e9ca104dbf57650824b4e6a3
Matthew Robinson <matt_robinson1@yahoo.com>
2020-01-06 19:01:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function map (arr, func) {
 
 function reduce (arr, func, startingValue) {
     let final = (startingValue) ? startingValue : arr[0]
-    
+    let i = (startingValue) ? 0 : 1
 
     for (; i < arr.length; i ++) {
         final = func(arr[i], final)
